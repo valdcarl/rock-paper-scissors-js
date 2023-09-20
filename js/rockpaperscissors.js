d6c06c2a1afe34ef05492ec7a24840f49b5dfef1
Carlos Valdez <valdezcarlos.cs@gmail.com>
2023-09-19 19:52:50 -0600

    Finished game function, first to 5 wins!

--- js/rockpaperscissors.js
+++ js/rockpaperscissors.js
@@ -48,14 +48,41 @@
             return (playerSelection == 'rock') ? "YOU WIN!" : "YOU LOSE!";
         }
     }
+
+    function game() {
+        /**
+         * A game is successfully completed when the player or computer reaches 5 wins
+         */
+        let playerCounter = 0;
+        let computerCounter = 0;
+
+        while (playerCounter < 5 && computerCounter < 5) {
+            // then the game should continue on
+            const computerChoice = getComputerChoice(choices);
+            const userChoice = playerChoice();
+            
+            const result = playSingleRound(userChoice, computerChoice);
+            console.log(result);
+
+            if (result.includes("YOU WIN!")) {
+                playerCounter++;
+            } else if (result.includes("YOU LOSE!")) {
+                computerCounter++;
+            }
+            
+            console.log(`Player Score: ${playerCounter}`);
+            console.log(`Computer Score: ${computerCounter}`);
+        }
+
+        if (playerCounter >= 5) {
+            console.log("PLAYER WINS THE GAME!")
+        } else if (computerCounter >= 5) {
+            console.log("COMPUTER WINS THE GAME!")
+        }
+        console.log("GOOD GAME!");
+    }
     
     // TESTING OUTPUTS
-    const computerChoice = getComputerChoice(choices);
-    console.log(computerChoice);
-    const userChoice = playerChoice();
-    console.log(userChoice);
-
-    const result = playSingleRound(userChoice, computerChoice);
-    console.log(result);
+    game();
     
 // END rockpaperscissors.js
